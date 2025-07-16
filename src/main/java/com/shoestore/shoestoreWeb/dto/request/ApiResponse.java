322915d1609fc36e4dcd1a54574eda711c23ee20
trungdongle <ngu0ic0dedao1@gmail.com>
2025-07-16 14:46:17 +0700

    adding signer key, error codes

--- src/main/java/com/shoestore/shoestoreWeb/dto/request/ApiResponse.java
+++ src/main/java/com/shoestore/shoestoreWeb/dto/request/ApiResponse.java
@@ -11,7 +11,7 @@ import lombok.experimental.FieldDefaults;
 @FieldDefaults(level = AccessLevel.PRIVATE)
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public class ApiResponse<T> {
-    int code;
+    int code = 1000;
     String message;
     T result;
 }
--- src/main/java/com/shoestore/shoestoreWeb/exception/ErrorCode.java
+++ src/main/java/com/shoestore/shoestoreWeb/exception/ErrorCode.java
@@ -5,7 +5,9 @@ public enum ErrorCode {
     INVALID_KEY(1000, "Uncategorized error"),
     USER_EXISTED(1001, "User existed"),
     USERNAME_INVALID(1002, "Username must be at least 3 character"),
-    PASSWORD_INVALID(1003, "Password must be at least 8 character")
+    PASSWORD_INVALID(1003, "Password must be at least 8 character"),
+    EMAIL_NOT_EXISTED(1004, "Email not existed"),
+    UNAUTHORIZED(1005, "User is not authorized")
     ;
     private final int code;
     private final String message;
